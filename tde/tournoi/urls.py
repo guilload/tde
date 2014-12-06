47466e0cb2ddbdfc00b695a6176732cea633d015
Adrien Guillo <adrien.guillo@gmail.com>
2014-12-05 19:31:54 -0500

    Fix login/logout routes

--- tde/tournoi/urls.py
+++ tde/tournoi/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.index_v, name='index'),
     url(r'^leaderboard$', views.leaderboard_v, name='leaderboard'),
-    url(r'^login$', views.login_v, name='login'),
-    url(r'^logout$', views.logout_v, name='logout'),
+    url(r'^login', views.login_v, name='login'),
+    url(r'^logout', views.logout_v, name='logout'),
     url(r'^(?P<season>\d+)/(?P<week>\d+)$', views.fixture_v, name='fixture')
 )
